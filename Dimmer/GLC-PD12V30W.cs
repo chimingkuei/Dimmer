8c030f1d32cbad022daf70c36d04b3568cf1bf5b
chimingkuei <chimingkuei@gmail.com>
2026-01-17 00:54:47 +0800

    Dimmer Ver.1.0.0.0

---
+++ Dimmer/GLC-PD12V30W.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Dimmer
+{
+    class PD12V30W
+    {
+
+        public SerialPort serialPort = new SerialPort();
+
+        /// <summary>
+        /// PD12V30W("COM5", 19200, 8, 0, 1);
+        /// </summary>
+        public PD12V30W(string _PortName, int _BaudRate, int _DataBits, int _Parity, int _StopBits)
+        {
+            serialPort.PortName = _PortName;
+            serialPort.BaudRate = _BaudRate;
+            serialPort.DataBits = _DataBits;
+            serialPort.Parity = (Parity)_Parity;
+            serialPort.StopBits = (StopBits)_StopBits;
+        }
+
+        public void Connect()
+        {
+            try
+            {
+                serialPort.Open();
+            }
+            catch
+            {
+                MessageBox.Show("連線失敗!", "警告");
+            }
+
+        }
+
+        public void DisConnect()
+        {
+            serialPort.Close();
+        }
+
+        private string OneChannelLRC(int led_value, int ch)
+        {
+            string Temp = (1 + 6 + ch + led_value).ToString("X");
+            string Data_sum = Temp.Length >= 2 ? Temp.Substring(Temp.Length - 2, 2) : (1 + 6 + ch + led_value).ToString("X2");
+            string LRC = (255 - Convert.ToInt32(Data_sum, 16) + 1).ToString("X2");
+            return LRC;
+        }
+
+        private string OneChannelProtocalFormat(int led_value, int ch)
+        {
+            string Header = ":";
+            string command = "0106000" + ch.ToString() + "00" + led_value.ToString("X2");
+            string LRC = OneChannelLRC(led_value, ch);
+            string msg = Header + command + LRC + "\r\n";
+            //Console.WriteLine(msg);
+            return msg;
+        }
+
+        public void OneChannelSetBrightness(int led_value, int ch)
+        {
+            string msg = OneChannelProtocalFormat(led_value, ch);
+            byte[] buf = Encoding.Default.GetBytes(msg);
+            serialPort.Write(buf, 0, buf.Length);
+        }
+
+        private string TwoChannelLRC(int led1_value, int led2_value)
+        {
+            string Temp = (1 + 16 + 1 + 2 + 4 + led1_value + led2_value).ToString("X");
+            string Data_sum = Temp.Length >= 2 ? Temp.Substring(Temp.Length - 2, 2) : (1 + 16 + 1 + 2 + 4 + led1_value + led2_value).ToString("X2");
+            string LRC = (255 - Convert.ToInt32(Data_sum, 16) + 1).ToString("X2");
+            return LRC;
+        }
+
+        private string TwoChannelProtocalFormat(int led1_value, int led2_value)
+        {
+            string Header = ":";
+            string command = "01100001000204" + led1_value.ToString("X2") + led2_value.ToString("X2");
+            string LRC = TwoChannelLRC(led1_value, led2_value);
+            string msg = Header + command + LRC + "\r\n";
+            //Console.WriteLine(msg);
+            return msg;
+        }
+
+        public void TwoChannelSetBrightness(int led1_value, int led2_value)
+        {
+            string msg = TwoChannelProtocalFormat(led1_value, led2_value);
+            byte[] buf = Encoding.Default.GetBytes(msg);
+            serialPort.Write(buf, 0, buf.Length);
+        }
+
+        #region Upgrade
+        private string OneChannel(int led_value, int ch)
+        {
+            string Temp = (1 + 6 + ch + led_value).ToString("X");
+            return Temp.Length >= 2 ? Temp.Substring(Temp.Length - 2, 2) : (1 + 6 + ch + led_value).ToString("X2");
+        }
+
+        private string TwoChannel(int led1_value, int led2_value)
+        {
+            string Temp = (1 + 16 + 1 + 2 + 4 + led1_value + led2_value).ToString("X");
+            return Temp.Length >= 2 ? Temp.Substring(Temp.Length - 2, 2) : (1 + 16 + 1 + 2 + 4 + led1_value + led2_value).ToString("X2");
+        }
+
+        private string LRCCall(int led_value, int ch_or_ledvalue, Func<int, int, string> fun)
+        {
+            return (255 - Convert.ToInt32(fun(led_value, ch_or_ledvalue), 16) + 1).ToString("X2");
+        }
+
+        private string ProtocalFormat(int led_value, int ch_or_ledvalue, int type)
+        {
+            string Header = ":";
+            string command = "";
+            string LRC = "";
+            if (type == 1)
+            {
+                command = "0106000" + ch_or_ledvalue.ToString() + "00" + led_value.ToString("X2");
+                LRC = LRCCall(led_value, ch_or_ledvalue, OneChannel);
+            }
+            else if (type == 2)
+            {
+                command = "01100001000204" + led_value.ToString("X2") + ch_or_ledvalue.ToString("X2");
+                LRC = LRCCall(led_value, ch_or_ledvalue, TwoChannel);
+            }
+            return Header + command + LRC + "\r\n";
+        }
+
+        public void SetBrightness(int led_value, int ch_or_ledvalue, int type)
+        {
+            string msg = ProtocalFormat(led_value, ch_or_ledvalue, type);
+            byte[] buf = Encoding.Default.GetBytes(msg);
+            serialPort.Write(buf, 0, buf.Length);
+        }
+        #endregion
+
+    }
+}
---
+++ Dimmer/MainWindow.xaml.cs
@@ -0,0 +1,319 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dimmer
+{
+    enum ProductModel
+    {
+        GLCPD12V30W, GLCPD24V24W, GLCLFB2350
+    }
+    enum ChannelControl
+    {
+        OneCh, TwoCh, FourCh
+    }
+
+    public class Parameter
+    {
+        public int LED1_val { get; set; }
+        public int LED2_val { get; set; }
+        public int LED3_val { get; set; }
+        public int LED4_val { get; set; }
+        public bool Ch1_Rd_val { get; set; }
+        public bool Ch2_Rd_val { get; set; }
+        public bool Ch3_Rd_val { get; set; }
+        public bool Ch4_Rd_val { get; set; }
+        public bool OneCh_val { get; set; }
+        public bool TwoCh_val { get; set; }
+        public bool FourCh_val { get; set; }
+        public bool One_Channel_val { get; set; }
+        public bool Two_Channel_val { get; set; }
+        public bool GLC_PD12V30W_val { get; set; }
+        public bool GLC_PD24V24W_val { get; set; }
+        public bool GLC_LFB2350_val { get; set; }
+        public string PortName_val { get; set; }
+        public int BaundRate_val { get; set; }
+        public int ByteSize_val { get; set; }
+        public int Parity_val { get; set; }
+        public int StopBit_val { get; set; }
+    }
+    
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Function
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("請問是否要關閉？", "確認", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private ProductModel SelectModel()
+        {
+            if ((bool)GLC_PD12V30W.IsChecked)
+            {
+                selectedModel = ProductModel.GLCPD12V30W;
+            }
+            else if ((bool)GLC_PD24V24W.IsChecked)
+            {
+                selectedModel = ProductModel.GLCPD24V24W;
+            }
+            else if ((bool)GLC_LFB2350.IsChecked)
+            {
+                selectedModel = ProductModel.GLCLFB2350;
+            }
+            return selectedModel;
+        }
+
+        private ChannelControl SelectChannel()
+        {
+            if ((bool)OneCh.IsChecked)
+            {
+                selectedChannel = ChannelControl.OneCh;
+            }
+            else if ((bool)TwoCh.IsChecked)
+            {
+                selectedChannel = ChannelControl.TwoCh;
+            }
+            else if ((bool)FourCh.IsChecked)
+            {
+                selectedChannel = ChannelControl.FourCh;
+            }
+            return selectedChannel;
+        }
+
+        #region Config
+        private void LoadConfig()
+        {
+            List<Parameter> Parameter_info = Config.Load();
+            LED1.Text = Parameter_info[0].LED1_val.ToString();
+            LED2.Text = Parameter_info[0].LED2_val.ToString();
+            LED3.Text = Parameter_info[0].LED3_val.ToString();
+            LED4.Text = Parameter_info[0].LED4_val.ToString();
+            Ch1_Rd.IsChecked = Parameter_info[0].Ch1_Rd_val;
+            Ch2_Rd.IsChecked = Parameter_info[0].Ch2_Rd_val;
+            Ch3_Rd.IsChecked = Parameter_info[0].Ch3_Rd_val;
+            Ch4_Rd.IsChecked = Parameter_info[0].Ch4_Rd_val;
+            OneCh.IsChecked = Parameter_info[0].OneCh_val;
+            TwoCh.IsChecked = Parameter_info[0].TwoCh_val;
+            FourCh.IsChecked = Parameter_info[0].FourCh_val;
+            GLC_PD12V30W.IsChecked = Parameter_info[0].GLC_PD12V30W_val;
+            GLC_PD24V24W.IsChecked = Parameter_info[0].GLC_PD24V24W_val;
+            GLC_LFB2350.IsChecked = Parameter_info[0].GLC_LFB2350_val;
+            PortName.Text = Parameter_info[0].PortName_val.ToString();
+            BaundRate.Text = Parameter_info[0].BaundRate_val.ToString();
+            ByteSize.Text = Parameter_info[0].ByteSize_val.ToString();
+            Parity.Text = Parameter_info[0].Parity_val.ToString();
+            StopBit.Text = Parameter_info[0].StopBit_val.ToString();
+        }
+
+        private void SaveConfig()
+        {
+            List<Parameter> Parameter_config = new List<Parameter>()
+                        {
+                            new Parameter() { LED1_val = Convert.ToInt32(LED1.Text),
+                                              LED2_val = Convert.ToInt32(LED2.Text),
+                                              LED3_val = Convert.ToInt32(LED3.Text),
+                                              LED4_val = Convert.ToInt32(LED4.Text),
+                                              Ch1_Rd_val = (bool)Ch1_Rd.IsChecked,
+                                              Ch2_Rd_val = (bool)Ch2_Rd.IsChecked,
+                                              Ch3_Rd_val = (bool)Ch3_Rd.IsChecked,
+                                              Ch4_Rd_val = (bool)Ch4_Rd.IsChecked,
+                                              OneCh_val = (bool)OneCh.IsChecked,
+                                              TwoCh_val = (bool)TwoCh.IsChecked,
+                                              FourCh_val = (bool)FourCh.IsChecked,
+                                              GLC_PD12V30W_val = (bool)GLC_PD12V30W.IsChecked,
+                                              GLC_PD24V24W_val = (bool)GLC_PD24V24W.IsChecked,
+                                              GLC_LFB2350_val = (bool)GLC_LFB2350.IsChecked,
+                                              PortName_val=PortName.Text,
+                                              BaundRate_val = Convert.ToInt32(BaundRate.Text),
+                                              ByteSize_val = Convert.ToInt32(ByteSize.Text),
+                                              Parity_val = Convert.ToInt32(Parity.Text),
+                                              StopBit_val = Convert.ToInt32(StopBit.Text)
+                            }
+                        };
+            Config.Save(Parameter_config);
+            Logger.WriteLog("儲存參數!", 1, richTextBoxGeneral);
+        }
+        #endregion
+        #endregion
+
+        #region Parameter and Init
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadConfig();
+        }
+        BaseLogRecord Logger = new BaseLogRecord();
+        BaseConfig<Parameter> Config = new BaseConfig<Parameter>();
+        GLCPD12V30W GLCPD12V30W;
+        GLCPD24V24W GLCPD24V24W;
+        GLCLFB2350 GLCLFB2350;
+        ProductModel selectedModel;
+        ChannelControl selectedChannel;
+        #endregion
+
+        #region Main Screen
+        private void Main_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as Button).Name)
+            {
+                case nameof(Connect):
+                    {
+                        SelectModel();
+                        switch (selectedModel)
+                        {
+                            case ProductModel.GLCPD12V30W:
+                                GLCPD12V30W = new GLCPD12V30W(PortName.Text, Convert.ToInt32(BaundRate.Text), Convert.ToInt32(ByteSize.Text), Convert.ToInt32(Parity.Text), Convert.ToInt32(StopBit.Text));
+                                GLCPD12V30W.Connect();
+                                break;
+
+                            case ProductModel.GLCPD24V24W:
+                                GLCPD24V24W = new GLCPD24V24W(PortName.Text, Convert.ToInt32(BaundRate.Text), Convert.ToInt32(ByteSize.Text), Convert.ToInt32(Parity.Text), Convert.ToInt32(StopBit.Text));
+                                GLCPD24V24W.Connect();
+                                break;
+
+                            case ProductModel.GLCLFB2350:
+                                GLCLFB2350 = new GLCLFB2350(PortName.Text, Convert.ToInt32(BaundRate.Text), Convert.ToInt32(ByteSize.Text), Convert.ToInt32(Parity.Text), Convert.ToInt32(StopBit.Text));
+                                GLCLFB2350.Connect();
+                                break;
+                        }
+                        break;
+                    }
+                case nameof(DisConnect):
+                    {
+                        SelectModel();
+                        switch (selectedModel)
+                        {
+                            case ProductModel.GLCPD12V30W:
+                                GLCPD12V30W.DisConnect();
+                                break;
+
+                            case ProductModel.GLCPD24V24W:
+                                break;
+
+                            case ProductModel.GLCLFB2350:
+                                GLCLFB2350.DisConnect();
+                                break;
+                        }
+                        break;
+                    }
+                case nameof(Light_Up):
+                    {
+                        SelectModel();
+                        SelectChannel();
+                        switch (selectedModel)
+                        {
+                            case ProductModel.GLCPD12V30W:
+                                switch (selectedChannel)
+                                {
+                                    case ChannelControl.OneCh:
+                                        if ((bool)Ch1_Rd.IsChecked)
+                                        {
+                                            GLCPD12V30W.OneChannelSetBrightness(Convert.ToInt32(LED1.Value), 1);
+                                        }
+                                        else if ((bool)Ch2_Rd.IsChecked)
+                                        {
+                                            GLCPD12V30W.OneChannelSetBrightness(Convert.ToInt32(LED2.Value), 2);
+                                        }
+                                        else if ((bool)Ch3_Rd.IsChecked)
+                                        {
+                                            GLCPD12V30W.OneChannelSetBrightness(Convert.ToInt32(LED3.Value), 3);
+                                        }
+                                        else if ((bool)Ch4_Rd.IsChecked)
+                                        {
+                                            GLCPD12V30W.OneChannelSetBrightness(Convert.ToInt32(LED4.Value), 4);
+                                        }
+                                        break;
+                                    case ChannelControl.TwoCh:
+                                        GLCPD12V30W.TwoChannelSetBrightness(Convert.ToInt32(LED1.Value), Convert.ToInt32(LED2.Value));
+                                        break;
+                                    case ChannelControl.FourCh:
+                                        GLCPD12V30W.FourChannelSetBrightness(Convert.ToInt32(LED1.Value), Convert.ToInt32(LED2.Value), Convert.ToInt32(LED3.Value), Convert.ToInt32(LED4.Value));
+                                        break;
+                                }
+                                break;
+                            case ProductModel.GLCPD24V24W:
+                                switch (selectedChannel)
+                                {
+                                    case ChannelControl.OneCh:
+                                        if ((bool)Ch1_Rd.IsChecked)
+                                        {
+                                            GLCPD24V24W.OneChannelSetBrightness(Convert.ToInt32(LED1.Value), 1);
+                                        }
+                                        else if ((bool)Ch2_Rd.IsChecked)
+                                        {
+                                            GLCPD24V24W.OneChannelSetBrightness(Convert.ToInt32(LED2.Value), 2);
+                                        }
+                                        else if ((bool)Ch3_Rd.IsChecked)
+                                        {
+                                            GLCPD24V24W.OneChannelSetBrightness(Convert.ToInt32(LED3.Value), 3);
+                                        }
+                                        else if ((bool)Ch4_Rd.IsChecked)
+                                        {
+                                            GLCPD24V24W.OneChannelSetBrightness(Convert.ToInt32(LED4.Value), 4);
+                                        }
+                                        break;
+                                    case ChannelControl.TwoCh:
+                                        GLCPD24V24W.TwoChannelSetBrightness(Convert.ToInt32(LED1.Value), Convert.ToInt32(LED2.Value));
+                                        break;
+                                    case ChannelControl.FourCh:
+                                        GLCPD24V24W.FourChannelSetBrightness(Convert.ToInt32(LED1.Value), Convert.ToInt32(LED2.Value), Convert.ToInt32(LED3.Value), Convert.ToInt32(LED4.Value));
+                                        break;
+                                }
+                                break;
+                            case ProductModel.GLCLFB2350:
+                                switch (selectedChannel)
+                                {
+                                    case ChannelControl.OneCh:
+                                        GLCLFB2350.OneChannelSetBrightness(Convert.ToInt32(LED1.Value), 1);
+                                        break;
+                                    case ChannelControl.TwoCh:
+                                        MessageBox.Show("This product model only has 1 channel!", "Confirm", MessageBoxButton.OK, MessageBoxImage.Question);
+                                        break;
+                                    case ChannelControl.FourCh:
+                                        MessageBox.Show("This product model only has 1 channel!", "Confirm", MessageBoxButton.OK, MessageBoxImage.Question);
+                                        break;
+                                }
+                                break;
+                        }
+                        break;
+                    }
+                case nameof(Save_Config):
+                    {
+                        SaveConfig();
+                        break;
+                    }
+            }
+        }
+        #endregion
+
+
+    }
+}
